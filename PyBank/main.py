9b49ac029e61d6e4d000e5442aed630cde2b8d4c
jacobdol <jacob.d@dokosens.com>
2023-09-16 17:34:27 -0500

    initial calculation of stats. TODO: average

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,64 @@
 import csv
 import os
+
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Report header
+header ="""
+--------------------------------------------------------
+
+      Financial Analysis
+      
+--------------------------------------------------------
+      """
+print(header)
+# Define a counter for months
+count_months = 0
+# Define a variable for profit/loss
+total_profit_loss = 0
+#months array
+months = []
+#profit array
+profit_loss = []
+#array to store monthly change
+change = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
+
+    for row in csvreader:
+        count_months +=1
+        #populate months list 
+        months.append(row[0])
+        #convert profit/loss to int
+        profit_loss_int = int(row[1])
+        #populate profit_loss
+        profit_loss.append(profit_loss_int)
+        # add profit/loss 
+        total_profit_loss += profit_loss_int
+#populate array with monthly change
+for i in range(len(profit_loss)-1):
+    change.append(profit_loss[i+1] - profit_loss[i])
+
+#calculate max increase in profits
+max_change_i = 0
+min_change_i = 0
+max_change = change[0]
+min_change = change[0]
+
+
+for i in range(len(change)-1):
+    if(max_change < change[i+1]):
+        max_change = change[i+1]
+        max_change_i = i
+    
+    if(min_change > change[i+1]):
+        min_change = change[i+1]
+        min_change_i = i
+
+
+print(f"Total Months: {count_months}")
+print(f"Total: {total_profit_loss}")
+print(f"Greatest Increase in Profits: {months[max_change_i+2]} {max_change}")
+print(f"Greatest Decrease in Profits: {months[min_change_i+2]} {min_change}")
