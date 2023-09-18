260521b60187bd0f7b3315bcf423353143ac99a6
jacobdol <jacob.d@dokosens.com>
2023-09-18 09:53:03 -0500

    summary and txt output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,17 +2,15 @@ import csv
 import os
 
 csvpath = os.path.join('Resources', 'election_data.csv')
-
+#separator
+separator = "------------------------------------------------"
 # Report header
-header ="""
-
-      Election Results
-      
---------------------------------------------------------
-      """
+header = "Election Results"
 total_votes = 0
 #create a dictionary where we will store votes per candidate
 votes_counter = {}
+#a string to output total votes per candidate
+vote_totals = ""
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
@@ -28,6 +26,25 @@ with open(csvpath) as csvfile:
             #otherwise create a new dictionary porperty with new key and value 1
             votes_counter[row[2]] = 1
 
-print(f"total votes {votes_counter}")
+# find a key with max votes
+max_key = max(votes_counter, key=lambda k: votes_counter[k])
 
-print(f"Total Votes: {total_votes: ,} \n")
+for key, value in votes_counter.items():
+    vote_totals += f"{key}: {value/total_votes:.3%} ({value:,}) \n\n"
+
+#buld summary
+summary = f"""
+{header}\n
+{separator}\n
+Total Votes: {total_votes: ,}\n
+{separator} \n
+{vote_totals}{separator}\n
+Winner: {max_key}\n
+{separator}
+"""
+print(summary)
+
+# write results in a .txt file
+f = open("./analysis/myfile.txt", "w")
+f.write(summary)
+f.close()
